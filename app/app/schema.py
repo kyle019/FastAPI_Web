32603383aea745960f4b8e27830adeceecfc7a91
kyle019 <71164084+kyle019@users.noreply.github.com>
2024-06-12 15:05:55 +0900

    Add files via upload

---
+++ app/app/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    name: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserPost(UserBase):
+    id: int
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
